8c947365f6a62082ba7770e271ba259c8771817b
Lisa L <lilas.lisa@hotmail.com>
2022-02-03 08:54:51 +0100

    Photographer page: fix bug (images/videos)

--- scripts/templates/ThumbMedia.js
+++ scripts/templates/ThumbMedia.js
@@ -1,25 +1,25 @@
-class ThumbVideo{
-    constructor(infosMedia){
-        this._infosMedia = infosMedia;
-    }
+// class ThumbVideo{
+//     constructor(infosMedia){
+//         this._infosMedia = infosMedia;
+//     }
 
-    createThumbVideo(){
-        const wrapperThumbMedia = document.createElement('div')
-        const thumbVideo = `<video src="${this._infosMedia.video}"></video>`
-        wrapperThumbMedia.innerHTML = thumbVideo
-        return wrapperThumbMedia
-    }
-}
+//     createThumbVideo(){
+//         const wrapperThumbMedia = document.createElement('div')
+//         const thumbVideo = `<video src="${this._infosMedia.video}"></video>`
+//         wrapperThumbMedia.innerHTML = thumbVideo
+//         return wrapperThumbMedia
+//     }
+// }
 
-class ThumbImg{
-    constructor(infosMedia){
-        this._infosMedia = infosMedia;
-    }
+// class ThumbImg{
+//     constructor(infosMedia){
+//         this._infosMedia = infosMedia;
+//     }
 
-    createThumbImg(){
-        const wrapperThumbMedia = document.createElement('div')
-        const thumbImg = `<img src="${this._infosMedia.image}"></img>`
-        wrapperThumbMedia.innerHTML = thumbImg
-        return wrapperThumbMedia
-    }
-}
+//     createThumbImg(){
+//         const wrapperThumbMedia = document.createElement('div')
+//         const thumbImg = `<img src="${this._infosMedia.image}"></img>`
+//         wrapperThumbMedia.innerHTML = thumbImg
+//         return wrapperThumbMedia
+//     }
+// }
--- scripts/templates/ThumbMediaDetails.js
+++ scripts/templates/ThumbMediaDetails.js
@@ -4,15 +4,15 @@ class ThumbMediaDetails{
     }
 
     createThumbMediaDetails(){
-        constwrapperThumbMediaDetails = document.createElement('div')
-       wrapperThumbMediaDetails.classList.add('grid_thumb_details')
+        const wrapperThumbMediaDetails = document.createElement('div')
+        wrapperThumbMediaDetails.classList.add('grid_thumb_details')
         const thumbMediaDetails = `<p class='grid_thumb_title'>${this._infosMedia.title}</p>
         <button class='grid_thumb_likes' onclick='like()'>
         <p class='grid_thumb_likes_number'>${this._infosMedia.likes}</p>
         <img class='grid_thumb_likes_button' src='assets/icons/like_heart.svg' alt="j'aime" role='button' aria-hidden='true'></img>
         </button>
         `
-       wrapperThumbMediaDetails.innerHTML = thumbMediaDetails
-        returnwrapperThumbMediaDetails
+        wrapperThumbMediaDetails.innerHTML = thumbMediaDetails
+        return wrapperThumbMediaDetails
     }
 }
